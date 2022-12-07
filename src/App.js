caba88b82f959fd8b93b8131111c7bf8d497358e
neriiavr <avrupalineriman@gmail.com>
2022-12-07 01:49:36 +0100

    css implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Dice from'./Dice.js';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <div className="dice-container">
+        <Dice name="1" />
+        <Dice name="2" />
+        <Dice name="3" />
+        <Dice name="4" />
+        <Dice name="5" />
+        <Dice name="5" />
+        <Dice name="6" />
+        <Dice name="1" />
+        <Dice name="2" />
+        <Dice name="3" />
     </div>
+    </main>
   );
 }
 
-export default App;
+
---
+++ src/Dice.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dice(props) {
+   return ( 
+    <div className="dice-face">
+        <h2 className="dice-num">{props.name}</h2>
+   </div>
+)}
+
